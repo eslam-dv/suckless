5637b5fbeeba160d845aebe55d8b8a3abc6027ca
Eslam Ebrahim <106819700+eslam-dv@users.noreply.github.com>
2025-01-02 19:12:34 +0200

    updates

--- dwm/config.h
+++ dwm/config.h
@@ -14,11 +14,11 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "RobotoMono Nerd Font Propo:size=10:style=Bold","monospace:size=10" };
 static const char dmenufont[]       = "RobotoMono Nerd Font Mono:size=10:style=Medium";
-static const char col_gray1[]       = "#2b2d3a";
+static const char col_gray1[]       = "#011423";
 static const char col_gray2[]       = "#7e8294";
-static const char col_gray3[]       = "#e1e3e4";
+static const char col_gray3[]       = "#CBE0F0";
 static const char col_gray4[]       = "#181a1c";
-static const char col_cyan[]        = "#6dcae8";
+static const char col_cyan[]        = "#24EAF7";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -34,14 +34,16 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class                  instance    title       tags mask     isfloating   monitor */
-	{ "zen-alpha",            NULL,       NULL,       1 << 1,       0,           -1 },
-  { "thunderbird-default",  NULL,       NULL,       1 << 3,       0,           -1 },
-  { "Mail",                 NULL,       NULL,       1 << 3,       0,           -1 },
-	{ "Pcmanfm",              NULL,       NULL,       1 << 2,       0,           -1 },
+	{ "zen-beta",             NULL,       NULL,       1 << 1,       0,           -1 },
+  { "Pcmanfm",              NULL,       NULL,       1 << 2,       0,           -1 },
+  { "discord",              NULL,       NULL,       1 << 3,       0,           -1 },
+  { "thunderbird-default",  NULL,       NULL,       1 << 4,       0,           -1 },
  	{ "Lxappearance",         NULL,       NULL,       0,            1,           -1 },
+ 	{ "mpv",                  NULL,       NULL,       0,            1,           -1 },
 	{ "Nitrogen",             NULL,       NULL,       0,            1,           -1 },
 	{ "Pavucontrol",          NULL,       NULL,       0,            1,           -1 },
   { "Xarchiver",            NULL,       NULL,       0,            1,           -1 },
+  { "Zotero",               NULL,       "Quick Format Citation",       0,            1,           -1 },
 };
 
 /* layout(s) */
@@ -71,7 +73,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]          = { "kitty", NULL };
+static const char *termcmd[]          = { "wezterm", NULL };
 static const char *browsercmd[]       = { "zen", NULL };
 static const char *filescmd[]         = { "pcmanfm", NULL };
 static const char *brightnessup[]     = { "light", "-A", "5", NULL };
